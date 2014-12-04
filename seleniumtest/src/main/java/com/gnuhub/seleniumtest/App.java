f58bb2e6737207c08acf5af8b2d034ccbe73382c
gnuhub <gnuhub@gmail.com>
2014-12-04 19:23:44 +0800

    get webdriver.firefox.bin from cli

--- seleniumtest/src/main/java/com/gnuhub/seleniumtest/App.java
+++ seleniumtest/src/main/java/com/gnuhub/seleniumtest/App.java
@@ -11,7 +11,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.setProperty("webdriver.firefox.bin", "/Users/stallman/Applications/Firefox.app/Contents/MacOS/firefox");
+    	String firefoxpath;
+    	firefoxpath = System.getProperty("firefox");
+        System.setProperty("webdriver.firefox.bin", firefoxpath);
         WebDriver driver;
     	driver = new FirefoxDriver();
     	driver.get("http://www.baidu.com/");
