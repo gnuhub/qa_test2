0fae772e40c9f1846a70df02c05725d0be8e6b3f
gnuhub <gnuhub@gmail.com>
2014-12-04 21:16:42 +0800

    login sugarcrm

--- seleniumtest/src/main/java/com/gnuhub/seleniumtest/App.java
+++ seleniumtest/src/main/java/com/gnuhub/seleniumtest/App.java
@@ -1,6 +1,8 @@
 package com.gnuhub.seleniumtest;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -19,5 +21,15 @@ public class App
         WebDriver driver;
     	driver = new FirefoxDriver();
     	driver.get(sugarurl);
+
+        WebElement user_name_input = driver.findElement(By.id("user_name"));
+        user_name_input.sendKeys("admin");
+
+        WebElement user_password_input = driver.findElement(By.id("user_password"));
+        user_password_input.sendKeys("asdf");
+
+        WebElement btn = driver.findElement(By.id("login_button"));
+        btn.click();
+
     }
 }
