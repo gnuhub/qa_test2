ab404a44a17f76896f5068d47fbd971dd262e5c7
gnuhub <gnuhub@gmail.com>
2014-12-04 19:09:48 +0800

    the first java example

--- seleniumtest/src/main/java/com/gnuhub/seleniumtest/App.java
+++ seleniumtest/src/main/java/com/gnuhub/seleniumtest/App.java
@@ -1,5 +1,8 @@
 package com.gnuhub.seleniumtest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.setProperty("webdriver.firefox.bin", "/Users/stallman/Applications/Firefox.app/Contents/MacOS/firefox");
+        WebDriver driver;
+    	driver = new FirefoxDriver();
+    	driver.get("http://www.baidu.com/");
     }
 }
