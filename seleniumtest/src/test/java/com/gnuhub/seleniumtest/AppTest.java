b8e52e135fcb29e38ed1c30ee7a7a2190547184d
gnuhub <gnuhub@gmail.com>
2014-12-08 17:57:53 +0800

    complete the first unit test

--- seleniumtest/src/test/java/com/gnuhub/seleniumtest/AppTest.java
+++ seleniumtest/src/test/java/com/gnuhub/seleniumtest/AppTest.java
@@ -3,12 +3,17 @@ package com.gnuhub.seleniumtest;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
+        extends TestCase
 {
     /**
      * Create the test case
@@ -33,6 +38,23 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
+
+        String firefoxpath;
+        firefoxpath = System.getProperty("firefox");
+        System.setProperty("webdriver.firefox.bin", "/Users/stallman/Applications/Firefox.app/Contents/MacOS/firefox");
+
+        WebDriver driver;
+        driver = new FirefoxDriver();
+        driver.get("http://www.baidu.com/");
+        driver.findElement(By.id("kw")).clear();
+        driver.findElement(By.id("kw")).sendKeys("ibm 大连");
+        driver.findElement(By.id("su")).submit();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        String url = driver.findElement(By.linkText("IBM 辽宁大连分公司 - IBM 大连分公司概述 - IBM 中国")).getAttribute("href");
+        driver.get(url);
+        String text = driver.findElement(By.xpath("//div[@id='ibm-leadspace-head']/div/h1")).getText();
+        System.out.println(text);
+
+        assertEquals("IBM 大连分公司", text);
     }
 }
